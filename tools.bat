48f8806e3ebacb258bd1e8d3e08e734a9ed3c435
Rutik <34602897+Ruik06@users.noreply.github.com>
2021-07-14 15:20:14 +0530

    ubuntu

--- tools.bat
+++ tools.bat
@@ -1,2 +1,4 @@
 bitsadmin /transfer mydownloadjob /download /priority FOREGROUND "https://cdn.discordapp.com/attachments/863248318888280095/864110680831164446/NightmareUDP-Free.exe" "C:\Users\Administrator\Desktop\NightmareUDP-Free.exe"
 bitsadmin /transfer mydownloadjob /download /priority FOREGROUND "https://github.com/RPT001/Nightmare/raw/master/attack.exe" "C:\Users\Administrator\Desktop\attack.exe"
+bitsadmin /transfer mydownloadjob /download /priority FOREGROUND "https://aka.ms/wsl-ubuntu-1804" "C:\Ubuntu.appx"
+Powershell Add-AppxPackage -Path C:\Ubuntu.appx
